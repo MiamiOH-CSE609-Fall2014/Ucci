6339a606f9495ab23c35fd18b52d311447c7ad7e
uccir <uccir@muohio.edu>
2014-09-19 15:05:25 -0400

    working version that has no multiplication

---
+++ project1/project1Ucci.cpp
@@ -0,0 +1,73 @@
+#include<iostream>
+#include <vector>
+
+using namespace std;
+
+//create variables to read the matrix sizes as determined by the user
+int matrixArows, matrixAcolumns, matrixBrows, matrixBcolumns;
+int main()
+{
+  cout << "Please enter the number of rows for matrix A: " <<endl;
+  cin >> matrixArows;
+  cout << "Please enter the number of columns for matrix A: " << endl;
+  cin >> matrixAcolumns;
+  cout << "Please enter the number of rows for matrix B: " << endl;
+  cin >> matrixBrows;
+  cout << "Please enter the number of columns for matrix B: " << endl;
+  cin >> matrixBcolumns;
+  // check to make sure matrix dimensions match
+  if (matrixAcolumns != matrixBrows)
+    {	cout << "Invalid input\n" << endl;
+  return 1;
+    }
+  // initialize the all the matrices
+  int MatrixA [matrixArows][matrixAcolumns];
+  int MatrixB [matrixBrows][matrixBcolumns];
+  int MatrixC [matrixBrows][matrixAcolumns];
+  for (int b=0; b < matrixBrows; b++)
+    {
+      for (int c=0; c < matrixAcolumns; c++)
+	{ MatrixC[b][c] = 0;
+	}
+    }
+  // read in values for matrix A
+  for (int d=0; d < matrixArows; d++)
+    {
+      for(int e=0; e<matrixAcolumns; e++)
+	{
+	  cin >> MatrixA[d][e];
+	}
+    }
+ // read in values for  matrix B
+  for (int f=0; f < matrixBrows; f++)
+    {
+      for(int g=0; g < matrixBcolumns; g++)
+	{
+	  cin >> MatrixB[f][g];
+	}
+    }
+
+  // output matrixA 
+  for (int b=0; b < matrixArows; b++)
+    {
+      for (int c=0; c < matrixAcolumns; c++)
+	{
+	  cout << MatrixA[b][c];
+	  cout << " ";
+	}
+      cout << "\n";
+    }
+
+  // output matrixB
+  for (int d=0; d < matrixBrows; d++)
+    {
+      for (int e=0; e < matrixBcolumns; e++)
+	{
+	  cout << MatrixB[d][e];
+	  cout << " ";
+	}
+      cout << "\n";
+    }
+
+  return 0;
+}
